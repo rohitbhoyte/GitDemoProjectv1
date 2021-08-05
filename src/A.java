be9dd3022aeddc8d384b0f3c139e6fd7362a5134
Rohit <rohitbhoyte@gmail.com>
2021-08-05 09:19:54 +0530

    FIRST COMMIT......

---
+++ src/A.java
@@ -0,0 +1,21 @@
+
+
+public class A {
+	
+public static void main(String[] args) {
+	int a=10;
+	for(int i=1;i<=a;i++)
+	{
+		if(a==i)
+		{
+			System.out.println("Condition is true");
+			int b=a*i;
+			System.out.println(b);
+		}else
+		{
+			System.out.println("Condition is fail");
+		}
+	}
+}
+	
+}
